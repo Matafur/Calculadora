adabe44d473d6f77297534c56cc87c0ee39b2d06
MT <miguel.t@nitolo.com>
2024-02-15 20:11:38 -0500

    responsive_ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'calculadora';
+  titulo = 'Mi calculadora';
   resultado: any = '0';
   operacionActual: string = '';
   OperacionAnteror: any = null;
